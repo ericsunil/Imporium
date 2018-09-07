f1664fac9c678a4990474e91e76da4c25134a489
Sunil Hyaunmikha <abinaskosansar@gmail.com>
2018-09-07 15:38:39 +0545

    OrderRequest Dispatch ko table ma data tanya + pair change gari enter garda save vayo but multiple huda melauna baki.
    Next task:  Order request dispatch ma multiple data huda edit gari enter garda data delete vako melauni. + TOTAL sum vako check.
    BillCustomer ma multiple product order garda redirect navako melauni.

--- ASPMVCIndraLaxmiImporium/ASPMVCIndraLaxmiImporium/Controllers/OrderRequestController.cs
+++ ASPMVCIndraLaxmiImporium/ASPMVCIndraLaxmiImporium/Controllers/OrderRequestController.cs
@@ -1,6 +1,7 @@
 ﻿using ASPMVCIndraLaxmiImporium.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -35,6 +36,72 @@ namespace ASPMVCIndraLaxmiImporium.Controllers
                 }
             }
             return View(emp);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult AddorEdit(Bill emp)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    if(emp.BillID != 0)
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    //if (emp.BillCustomerID == 0)
+                    //{
+                    //    db.BillCustomers.Add(emp);
+                    //    db.SaveChanges();
+                    //    DBModel isCommet = new DBModel();
+                    //    Bill a = isCommet.Bills.SingleOrDefault(b => b.BillNumber == emp.BillNumber);
+                    //    if (a != null)
+                    //    {
+                    //        a.IsCommit = true;
+                    //        isCommet.SaveChanges();
+                    //    }
+
+                    //}
+                    //else
+                    //{
+                    //    db.Entry(emp).State = EntityState.Modified;
+                    //    db.SaveChanges();
+                    //}
+
+                }
+                return RedirectToAction("AddorEdit");
+                //return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllBillCustomer()), message = "Save Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult SaveCustomerDetail(BillCustomer cust)
+        {
+            TransactionMain main = new TransactionMain() { TransactionMainID = 0, BillNumber = cust.BillNumber, Description = "Goods dispatch to " + cust.CustomerCode, Date = cust.Date, UserName = "Admin" };
+
+            DBModel db = new DBModel();
+            db.TransactionMains.Add(main);
+            db.SaveChanges();
+
+            //for TransactionDetail
+            DBModel db2 = new DBModel();
+
+             db2.TransactionDetails.Add(new TransactionDetail() { TransactionDetailID = 0, TransactionMainID = main.TransactionMainID, LedgerNumber = LedgerController.GetLedgerNumber((int)cust.CustomerCode), Description ="Goods dispatch by "+ cust.BillNumber , Debit = 0, Credit = Convert.ToDouble(cust.Total), CustomerID = (int)cust.CustomerCode });
+            db2.SaveChanges();
+
+            DBModel db3 = new DBModel();
+            db3.Entry(cust).State = EntityState.Modified;
+            db3.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
 
